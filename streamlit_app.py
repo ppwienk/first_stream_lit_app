7c803c09b6604c3a027c4fb5972b146818fb1336
Pietro <111339801+ppwienk@users.noreply.github.com>
2022-08-17 08:12:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-
+import pandas
 
 streamlit.header('menu')
 streamlit.text('friet')
@@ -7,5 +7,6 @@ streamlit.text('kroket')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 
